77459a8bc266f31ae6397727c3070dbeb2436ecd
BrandonWSmith <BWSmith09@gmail.com>
2024-08-19 03:05:55 -0400

    Finish Profile page

--- LFG/Pages/Profile.cshtml.cs
+++ LFG/Pages/Profile.cshtml.cs
@@ -3,6 +3,7 @@ using LFG.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace LFG.Pages
 {
@@ -16,10 +17,35 @@ namespace LFG.Pages
       _context = context;
     }
 
-    [BindProperty(SupportsGet = true)] public User User { get; set; }
+    public User User { get; set; }
+    public List<Platform> UserPlatforms { get; set; }
+    public List<string> UserPlatformNames { get; set; }
+    public List<Group> UserGroups { get; set; }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
+      User  = await _context.Users.FirstOrDefaultAsync(u => u.Username == RouteData.Values["username"]);
+
+      UserPlatforms = await _context.UsersPlatforms
+        .Where(p => p.UserId == User.Id)
+        .Include(p => p.Platform)
+        .Select(p => new Platform
+        {
+          Name = p.Platform.Name
+        })
+        .ToListAsync();
+
+      UserPlatformNames = UserPlatforms.Select(p => p.Name).ToList();
+
+      UserGroups = await _context.UsersGroups
+        .Where(g => g.UserId == User.Id)
+        .Include(g => g.Group)
+        .Select(g => new Group
+        {
+          Name = g.Group.Name,
+          AvatarId = g.Group.AvatarId
+        })
+        .ToListAsync();
     }
   }
 }

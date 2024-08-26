587ece331cef398c0b6bf3caf4b8927abdaaf063
Brandon Smith <bwsmith09@gmail.com>
2024-08-26 12:59:24 -0400

    Change Group pages toggle games function from form post to script function, fix incorrect routing for Group page link on Profile page

--- LFG/Pages/Group/Group.cshtml.cs
+++ LFG/Pages/Group/Group.cshtml.cs
@@ -14,18 +14,10 @@ namespace LFG.Pages.Group
         {
           _context = context;
         }
-
-        [BindProperty(SupportsGet = true)]
+        
         public Models.Group Group { get; set; }
-
-        [BindProperty(SupportsGet = true)]
         public User Owner { get; set; }
-
-        [BindProperty(SupportsGet = true)]
         public List<Game> GroupGames { get; set; }
-
-        [BindProperty(SupportsGet = true)]
-        public bool ShowGames { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -42,22 +34,5 @@ namespace LFG.Pages.Group
             })
             .ToListAsync();
         }
-
-        public async Task OnPostToggleGames()
-        {
-          ShowGames = ShowGames == false;
-          Group = await _context.Groups.FirstOrDefaultAsync(g => g.Name == RouteData.Values["groupname"]);
-          Owner = await _context.Users.FirstOrDefaultAsync(u => u.Id == Group.Owner);
-
-          GroupGames = await _context.GroupsGames
-            .Where(g => g.GroupId == Group.Id)
-            .Include(g => g.Game)
-            .Select(g => new Models.Game
-            {
-              Name = g.Game.Name,
-              CoverId = g.Game.CoverId
-            })
-            .ToListAsync();
-        }
-  }
+    }
 }
